d7dc4faf1d264624d7a5a5814bd0d62a79d36dbb
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-05-14 14:35:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ function setup() {
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,Modelloaded)
+    poseNet.on('pose', gotPoses);
+
     
     
+}
+function Modelloaded() {
+    
+    console.log("poseNet is Initialized");
+
 }
 function draw() {
     
@@ -20,5 +28,16 @@ function draw() {
 function take_snapshot() {
 
 save("Hatoclown.jpeg");
+    
+}
+function gotPoses(results) {
+
+    if (results.length>0) {
+        console.log(results);
+        console.log("nosex="+results[0].pose.nose.x);
+        console.log("nosey="+results[0].pose.nose.y);
+                
+    }
+
     
 }
